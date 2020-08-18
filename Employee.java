8650cc03f96b802582498af85338197bc91c5cf4
SRIJITH-G <srijithgovindarajan@gmail.com>
2020-08-18 10:09:26 +0530

    clone RepoChanges

--- Employee.java
+++ Employee.java
@@ -2,6 +2,7 @@ class Employee
  
  public static void main(String [] args)
  {
+	 int a =1;
 	 System.out.println("Employee Details");
-	 
+	 System.out.println("file no:"+a);
  }
--- TestEmployee.java
+++ TestEmployee.java
@@ -3,7 +3,8 @@ class TestEmployee
 public static void main(String [] args)
 {
 	int a = 101;
+	int b= 23
 	System.out.println("Employee ID :"+a);
 	System.out.println("Employee Name: SRIJTH");
-	
+		System.out.println("age:"+b);
 }
